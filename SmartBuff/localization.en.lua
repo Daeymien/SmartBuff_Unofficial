056a26a3cf482ddcc73adb4b9df868b07a52b62d
Daeymien <daeymien@gmail.com>
2020-10-15 13:25:51 -0400

    Update localization.en.lua
    Update "What's New"

--- SmartBuff/localization.en.lua
+++ SmartBuff/localization.en.lua
@@ -4,8 +4,8 @@
 
 -- Whats new info
 SMARTBUFF_WHATSNEW = "|cffffffffWhat's new:|r\n\n"
-  .."- Bump for Shadowlands compatibility\n\n"
-  .."- Added a couple of toys and an Augment Rune.\n\n"
+  .."- Added back Instant Poison for Outlaw Rogues\n\n"
+  .."- Added back Windfury and Flametongue Weapons for Shaman\n\n"
   
   --[[
   .."- Thanks for donation:\n"
