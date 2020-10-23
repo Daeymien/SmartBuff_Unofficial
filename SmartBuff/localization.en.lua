7582d08e82477ee03131e68ef34050c72e5b34b4
Daeymien <daeymien@gmail.com>
2020-10-23 17:28:33 -0400

    Version bump

--- SmartBuff/localization.en.lua
+++ SmartBuff/localization.en.lua
@@ -12,6 +12,7 @@ SMARTBUFF_WHATSNEW = "|cffffffffWhat's new:|r\n\n"
   .."- Re-add Water Shield for Resto Shaman\n\n"
   .."- Re-adds Instant Poison for Outlaw Rogues\n\n"
   .."- Re-adds Windfury Weapon and Flametongue Weapon for Shaman\n\n"
+  .."- Add some missing BfA stat foods.\n\n"
 
   
   --[[
